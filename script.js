da062881a0b1c983fbd57ccac1af8c3ca2c171a5
Ilya Kutsenko <viminiko@gmail.com>
2025-01-06 23:49:31 +0200

    add files

---
+++ script.js
@@ -0,0 +1,29 @@
+const ball = document.getElementById('ball')
+const field = document.getElementById('field')
+
+field.addEventListener('click', function (event) {
+
+    const fieldRect = field.getBoundingClientRect();
+
+    const clickX = event.clientX - fieldRect.left - field.clientLeft;
+    const clickY = event.clientY - fieldRect.top - field.clientTop;
+
+    const ballWidth = ball.offsetWidth;
+    const ballHeight = ball.offsetHeight;
+
+    const newX = clickX - (ballWidth / 2)
+    const newY = clickY - (ballHeight / 2)
+
+    const maxX = field.offsetWidth - ballWidth
+    const maxY = field.offsetHeight - ballHeight
+
+    let limitedX = Math.max(0, Math.min(newX, maxX))
+    let limitedY = Math.max(0, Math.min(newY, maxY))
+
+     
+    ball.style.left = limitedX + 'px'
+    ball.style.top = limitedY + 'px'
+   
+    
+})
+
